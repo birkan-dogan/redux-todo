c49254a9b639b30e75422a24536df1bd9dda874b
birkan <b.561birkan@gmail.com>
2022-10-03 21:39:06 +0300

    creating store using reducer func createStore hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import "./App.css";
 import Todo from "./components/Todo";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import todoReducer from "./redux/reducers/todoReducers";
 function App() {
+  const store = createStore(todoReducer);
   return (
     <div className="app">
-      <Todo />
+      <Provider store={store}>
+        <Todo />
+      </Provider>
     </div>
   );
 }
