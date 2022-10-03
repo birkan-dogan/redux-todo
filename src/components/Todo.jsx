b95d4cce409f59098c1999b918e27f2cca289b14
birkan <b.561birkan@gmail.com>
2022-10-03 21:31:49 +0300

    TodoInput component is added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,13 @@
 import React from "react";
-
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
+import "./Todo.css";
 const Todo = () => {
   return (
-    <div>
-      <h1>Todo</h1>
+    <div className="app">
+      <h2>Todo With Redux</h2>
+      <TodoInput />
+      <TodoList />
     </div>
   );
 };
--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const TodoInput = () => {
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setText("");
+  };
   return (
-    <div>
-      <h1>todoInput</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add Todo"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="todo-input"
+      />
+      <button type="submit" className="add-button">
+        Add
+      </button>
+    </form>
   );
 };
+export default TodoInput;
