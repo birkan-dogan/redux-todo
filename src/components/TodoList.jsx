4172617dc8f56d616f690852d42e7d0364370ca2
birkan <b.561birkan@gmail.com>
2022-10-03 22:22:54 +0300

    TodoList component to display todos

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todoReducer);
+  return (
+    <div>
+      <div>
+        {todos?.map((todo) => (
+          <TodoItem key={todo.id} {...todo} />
+        ))}
+      </div>
+      <div className="clear-wrapper">
+        <button className="clear-button">Clear</button>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
