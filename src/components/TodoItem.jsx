48e8e53a88226f9250dce36a37f33c7793421993
birkan <b.561birkan@gmail.com>
2022-10-04 22:13:31 +0300

    regular and conditional styling

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -2,8 +2,12 @@ import React from "react";
 import okLogo from "../assets/ok.png";
 import deleteLogo from "../assets/delete.png";
 const TodoItem = ({ completed, text, id }) => {
+  const styled = {
+    textDecoration: completed ? "line-through" : "none",
+    backgroundColor: completed ? "#A9A9A9" : "orange",
+  };
   return (
-    <div>
+    <div style={styled} className="todo-list">
       <h2 className="todoText">{text}</h2>
       <div>
         <span>
